53231d629e5799bf37fd3a5aaa5db55aa70f3538
Hriday Bradoo <hriday.bradoo@kellogg.northwestern.edu>
2024-02-22 00:26:41 +0000

    hw3 part 1 final commit

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,29 @@
+class PlacesController < ApplicationController
+  def index
+    # find all places
+    @places = Place.all
+    # render places/index view
+  end
+
+
+def show
+  # find a place
+  @places = Place.find_by({"id" => params["id"]})
+end
+
+def new
+  # render view with new Company form
+end
+
+def create
+  # start with a new place
+  @place = Place.new
+  # assign user-entered form data to Place's columns
+  @place["name"] = params["name"]
+  
+  # save Place row
+  @place.save
+  # redirect user
+  redirect_to "/places"
+end
+end
